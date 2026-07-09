81bb5106dee39b5f88a36917c1c0872a53281910
Uzair Sayeed <uzairsayeed592@gmail.com>
2026-07-09 12:02:45 +0530

    Create 5.max_subarray_sum.py

---
+++ NeetCode150/Prefix_sum/5.max_subarray_sum.py
@@ -0,0 +1,38 @@
+# Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+# Output: 6
+# Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+# Example 2:
+
+# Input: nums = [1]
+# Output: 1
+# Explanation: The subarray [1] has the largest sum 1.
+# Example 3:
+
+# Input: nums = [5,4,-1,7,8]
+# Output: 23
+# Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 105
+# -104 <= nums[i] <= 104
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        res = float('-inf')
+        min_seen_so_far = float('inf')
+        prefix_sum = 0
+
+        for num in nums:
+            min_seen_so_far = min(min_seen_so_far, prefix_sum)
+            prefix_sum += num
+            res = max(res, prefix_sum - min_seen_so_far)
+
+        return res
