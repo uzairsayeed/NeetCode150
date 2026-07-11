6f167330994b6b2a8b21f2723a6b2787021823a5
Uzair Sayeed <uzairsayeed592@gmail.com>
2026-07-11 23:57:49 +0530

    Add optimized majority element finder
    Refactor majorityElement method and add Boyer-Moore Voting Algorithm implementation.

--- 6.Array_Techniques/2.majority_element.py
+++ 6.Array_Techniques/2.majority_element.py
@@ -27,7 +27,7 @@
 from collections import Counter
 class Solution:
     # TC = O(n), SC = O(n)
-    def majorityElement(self, nums: List[int]) -> int:
+    def majorityElement1(self, nums: List[int]) -> int:
         n = len(nums)
         majority_ele_freq = 0
         res = -1
@@ -36,14 +36,50 @@ class Solution:
         # print('freq_map --> ', freq_map)
         for ele, freq in freq_map.items():
             # print('ele, freq --> ', ele, freq)
-            if freq >= n//2:
+            if freq > n//2:
                 if freq > majority_ele_freq:
                     majority_ele_freq = freq
                     res = ele
 
         return res
+    
+    # Optimized Approach (Boyer-Moore Voting Algorithm)
+    # Intuition:
+        # The problem guarantees that a majority element always exists
+        # (frequency > n/2).
+        #
+        # Imagine repeatedly cancelling one occurrence of the candidate with one
+        # occurrence of any different element.
+        #
+        # Since the majority element appears more times than all other elements
+        # combined, it can never be completely cancelled. Therefore, after all
+        # possible cancellations, the remaining candidate must be the majority
+        # element.
+        #
+        # 'cnt' represents the number of unmatched occurrences of the current
+        # candidate after all cancellations performed so far.
+    # TC = O(n)
+    # SC = O(1)
+    # TC = O(n), SC = O(1)
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate = None
+        cnt = 0
+
+        for num in nums:
+            if cnt == 0:
+                candidate = num
+                cnt += 1
+            else:
+                if num == candidate:
+                    cnt += 1
+                else:
+                    cnt -= 1
+                    
+        return candidate
+
+
 o = Solution()
 # nums = [3,2,3]
-# nums = [2,2,1,1,1,2,2]
-nums = [6,5,5]
+nums = [2,2,1,1,1,2,2]
+# nums = [6,5,5]
 print(o.majorityElement(nums))
