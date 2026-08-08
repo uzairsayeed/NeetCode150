c7e394bdae4e153016196fd933a335a1bc5f7255
Uzair Sayeed <uzairsayeed592@gmail.com>
2026-08-08 12:02:52 +0530

    Update 5.max_subarray_sum.py

--- NeetCode150/Prefix_sum/5.max_subarray_sum.py
+++ NeetCode150/Prefix_sum/5.max_subarray_sum.py
@@ -50,3 +50,53 @@ class Solution:
             res = max(res, curr_sum)
 
         return res
+
+
+    # Day-7: 08/Aug/2026
+        # REcognityion Trigger: SUbarray Sum
+        # Pattern: Prefix Sum / Kadane's Algorithm
+        # Intuition:
+            # Prefix Sum Approach:
+                # Any subarray [l,r] sum can be represented as prefix_sum[r] - prefix_sum[l-1]
+                # In this problme we want : (preix_sum[r] - prefix_sum[l-1]) to be max
+                # => prefix_sum[r] can be MAX if we find prefix_sum[l-1] to be MIN
+                # So we compute : "min_prefix_sum_seen_so_far"
+                # Then we compute max_prefix_sum
+        # TC = O(n), SC = O(1)
+        # Mistakes Made:
+            # Needed help to trigger the intuition
+            # Rating : 3/5
+    def solve(self, nums: List[int]) -> int:
+        prefix_sum = 0
+        min_prefix_sum_seen_so_far = float('inf')
+        max_prefix_sum = float('-inf')
+
+        for idx,num in enumerate(nums):
+            min_prefix_sum_seen_so_far = min(min_prefix_sum_seen_so_far, prefix_sum)
+            prefix_sum += num
+            max_prefix_sum = max(max_prefix_sum, prefix_sum - min_prefix_sum_seen_so_far)
+            
+        return max_prefix_sum
+    
+    # Day-7: 08/Aug/2026
+        # REcognityion Trigger: SUbarray Sum
+        # Pattern: Prefix Sum / Kadane's Algorithm
+        # Intuition:
+            # Kadane's Algorithm:
+                # We compute running SUM
+                # At every idx, we have to options either to continue with the RUNNING SUM or just considere the CURR ELEMENT 
+                # Since nums can be negative as well, we need to pick the MAX of (RUNNING SUM, CURR ELEMENT)
+                # Then we compute the "max_prefix_sum"
+        # TC = O(n), SC = O(1)
+        # Mistakes Made:
+            # Needed help to trigger the intuition
+            # Rating : 3/5
+    def solve(self, nums: List[int]) -> int:
+        max_prefix_sum = float('-inf')
+        curr_sum = 0
+
+        for num in nums:
+            curr_sum = max(num, curr_sum + num)
+            max_prefix_sum = max(max_prefix_sum, curr_sum)
+
+        return max_prefix_sum
